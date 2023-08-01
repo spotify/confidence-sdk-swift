d5a13b2263d87e7e4df6d03c779b7c9e90092129
Brian Hackett <bhackett@spotify.com>
2023-08-01 16:36:42 +0100

    Updated demo app to use events

--- ConfidenceDemoApp/ConfidenceDemoApp/ContentView.swift
+++ ConfidenceDemoApp/ConfidenceDemoApp/ContentView.swift
@@ -2,31 +2,82 @@ import SwiftUI
 import OpenFeature
 
 struct ContentView: View {
+    @StateObject var status = Status()
     @StateObject var text = DisplayText()
     @StateObject var color = FlagColor()
 
     var body: some View {
-        VStack {
-            Image(systemName: "flag")
-                .imageScale(.large)
-                .foregroundColor(color.color)
-                .padding(10)
-            Text(text.text)
-            Button("Get remote flag value") {
-                text.text = OpenFeatureAPI
-                    .shared
-                    .getClient()
-                    .getStringValue(key: "hawkflag.color", defaultValue: "ERROR")
-                if text.text == "Green" {
-                    color.color = .green
-                } else if text.text == "Yellow" {
-                    color.color = .yellow
-                } else {
-                    color.color = .red
+        if case .ready = status.state {
+            VStack {
+                Image(systemName: "flag")
+                    .imageScale(.large)
+                    .foregroundColor(color.color)
+                    .padding(10)
+                Text(text.text)
+                Button("Get remote flag value") {
+                    text.text = OpenFeatureAPI
+                        .shared
+                        .getClient()
+                        .getStringValue(key: "hawkflag.color", defaultValue: "ERROR")
+                    if text.text == "Green" {
+                        color.color = .green
+                    } else if text.text == "Yellow" {
+                        color.color = .yellow
+                    } else {
+                        color.color = .red
+                    }
                 }
             }
+            .padding()
+        } else if case .error(let error) = status.state {
+            VStack {
+                Text("Provider Error")
+                Text(error?.localizedDescription ?? "An unknow error has occured.")
+                    .foregroundColor(.red)
+            }
+        } else {
+            VStack {
+                ProgressView()
+            }
         }
-        .padding()
+    }
+}
+
+class Status: ObservableObject {
+    enum State {
+        case unknown
+        case ready
+        case error(Error?)
+    }
+
+    @Published var state: State = .unknown
+
+    init() {
+        OpenFeatureAPI.shared.addHandler(
+            observer: self,
+            selector: #selector(confidenceReady(notification:)),
+            event: .ready
+        )
+
+        OpenFeatureAPI.shared.addHandler(
+            observer: self,
+            selector: #selector(confidenceError(notification:)),
+            event: .error
+        )
+    }
+
+    @objc func confidenceReady(notification: Notification) {
+        DispatchQueue.main.async {
+            self.state = .ready
+        }
+
+    }
+
+    @objc func confidenceError(notification: Notification) {
+        DispatchQueue.main.async {
+            let error = notification.userInfo?[providerEventDetailsKeyError] as? Error
+            self.state = .error(error)
+        }
     }
 }
 
