67c8285a68129871ff43b2db53477aa6549634fd
Fabrizio Demaria <fabrizio.f.demaria@gmail.com>
2024-04-11 18:21:52 +0200

    Fix CI build

--- Sources/ConfidenceProvider/Utils/BaseUrlMapper.swift
+++ Sources/ConfidenceProvider/Utils/BaseUrlMapper.swift
@@ -5,11 +5,11 @@ public enum BaseUrlMapper {
     static func from(region: ConfidenceRegion) -> String {
         switch region {
         case .global:
-            "https://resolver.confidence.dev/v1/flags"
+            return "https://resolver.confidence.dev/v1/flags"
         case .europe:
-            "https://resolver.eu.confidence.dev/v1/flags"
+            return "https://resolver.eu.confidence.dev/v1/flags"
         case .usa:
-            "https://resolver.us.confidence.dev/v1/flags"
+            return "https://resolver.us.confidence.dev/v1/flags"
         }
     }
 }
