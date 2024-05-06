624941707f3761463aac21e120dec28458b3e98a
vahid torkaman <vahidt@spotify.com>
2024-05-06 15:12:31 +0200

    handle app launch and app install and app updates

--- Sources/Confidence/ConfidenceAppLifecycleMonitor.swift
+++ Sources/Confidence/ConfidenceAppLifecycleMonitor.swift
@@ -30,6 +30,10 @@ public class ConfidenceAppLifecycleMonitor: ConfidenceEventProducer, ObservableO
         UIApplication.backgroundRefreshStatusDidChangeNotification
     ]
 
+    static var versionNameKey = "CONFIDENCE_VERSION_NAME_KEY"
+    static var buildNameKey = "CONFIDENCE_VERSIONN_KEY"
+    private var isLaunched = false
+
     public init() {
         for notification in appNotifications {
             NotificationCenter.default.addObserver(
@@ -45,24 +49,64 @@ public class ConfidenceAppLifecycleMonitor: ConfidenceEventProducer, ObservableO
         NotificationCenter.default.removeObserver(self)
     }
 
+    private func track(eventName: String, sourceApp: String = "", url: String = "") {
+        let previousBuild: String? = UserDefaults.standard.string(forKey: Self.buildNameKey)
+        let previousVersion: String? = UserDefaults.standard.string(forKey: Self.versionNameKey)
+
+        let currentVersion: String = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+        let currentBuild: String = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? ""
+
+        var message: ConfidenceStruct = [
+            "version": .init(string: currentVersion),
+            "build": .init(string: currentBuild)
+        ]
+
+        // handle referreres
+        if !sourceApp.isEmpty {
+            message.updateValue(ConfidenceValue.init(string: sourceApp), forKey: "source-app")
+        }
+        if !url.isEmpty {
+            message.updateValue(ConfidenceValue.init(string: url), forKey: "source-url")
+        }
+
+        if eventName == "app-launched" && !isLaunched {
+            isLaunched = true
+            if previousBuild != currentBuild || previousVersion != currentVersion {
+                events.send(Event(name: "app-updated", message: message))
+                return
+            } else {
+                events.send(Event(name: "app-installed", message: message))
+                return
+            }
+        }
+
+        events.send(Event(name: eventName, message: message))
+
+        UserDefaults.standard.setValue(currentVersion, forKey: Self.versionNameKey)
+        UserDefaults.standard.setValue(currentBuild, forKey: Self.buildNameKey)
+    }
+
     @objc func notificationResponse(notification: NSNotification) {
         switch notification.name {
         case UIApplication.didEnterBackgroundNotification:
-            events.send(Event(name: "enter-background"))
+            track(eventName: "enter-background")
         case UIApplication.willEnterForegroundNotification:
-            events.send(Event(name: "enter-foreground"))
+            track(eventName: "enter-foreground")
         case UIApplication.didFinishLaunchingNotification:
-            events.send(Event(name: "app-launched"))
+            let options = notification.userInfo as? [UIApplication.LaunchOptionsKey: Any] ?? nil
+            let sourceApp: String = options?[UIApplication.LaunchOptionsKey.sourceApplication] as? String ?? ""
+            let url: String = options?[UIApplication.LaunchOptionsKey.url] as? String ?? ""
+            track(eventName: "app-launch", sourceApp: sourceApp, url: url)
         case UIApplication.didBecomeActiveNotification:
-            events.send(Event(name: "app-active"))
+            track(eventName: "app-active")
         case UIApplication.willResignActiveNotification:
-            events.send(Event(name: "resign-active"))
+            track(eventName: "resign-active")
         case UIApplication.didReceiveMemoryWarningNotification:
-            events.send(Event(name: "memory-warning"))
+            track(eventName: "memory-warning")
         case UIApplication.significantTimeChangeNotification:
-            events.send(Event(name: "significant-time-change"))
+            track(eventName: "significant-time-change")
         case UIApplication.backgroundRefreshStatusDidChangeNotification:
-            events.send(Event(name: "bg-refresh-status-changed"))
+            track(eventName: "bg-refresh-status-changed")
         default:
             break
         }
