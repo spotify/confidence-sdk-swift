f3a41515e3a03eeab660321c0e79aa8c0a31845b
Fabrizio Demaria <fabrizio.f.demaria@gmail.com>
2024-05-10 14:27:51 +0200

    Make buffered passthrough serial

--- Sources/Confidence/BufferedPassthroughSubject.swift
+++ Sources/Confidence/BufferedPassthroughSubject.swift
@@ -1,24 +1,30 @@
+import Foundation
 import Combine
 
 class BufferedPassthrough<T> {
     private let subject = PassthroughSubject<T, Never>()
     private var buffer: [T] = []
     private var isListening = false
+    private let queue = DispatchQueue(label: "com.confidence.passthrough_serial")
 
     func send(_ value: T) {
-        if isListening {
-            subject.send(value)
-        } else {
-            buffer.append(value)
+        queue.sync {
+            if isListening {
+                subject.send(value)
+            } else {
+                buffer.append(value)
+            }
         }
     }
 
     func publisher() -> AnyPublisher<T, Never> {
-        isListening = true
-        let bufferedPublisher = buffer.publisher
-        buffer.removeAll()
-        return bufferedPublisher
-            .append(subject)
-            .eraseToAnyPublisher()
+        return queue.sync {
+            isListening = true
+            let bufferedPublisher = buffer.publisher
+            buffer.removeAll()
+            return bufferedPublisher
+                .append(subject)
+                .eraseToAnyPublisher()
+        }
     }
 }
