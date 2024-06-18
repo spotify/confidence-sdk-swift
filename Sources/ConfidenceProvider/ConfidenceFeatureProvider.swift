bf368df1fba8b545b22f16a901d1ebd7b5d6fde9
Fabrizio Demaria <fabrizio.f.demaria@gmail.com>
2024-06-18 10:20:44 +0200

    feat: CreateConfidence from Provider for Metadata (#141)
    * feat: SDK ID for native and OF
    
    * feat: Force correct init for Provider

--- Sources/ConfidenceProvider/ConfidenceFeatureProvider.swift
+++ Sources/ConfidenceProvider/ConfidenceFeatureProvider.swift
@@ -5,12 +5,14 @@ import OpenFeature
 import os
 
 struct Metadata: ProviderMetadata {
-    var name: String?
+    var name: String? = ConfidenceFeatureProvider.providerId
 }
 
 /// The implementation of the Confidence Feature Provider. This implementation allows to pre-cache evaluations.
 public class ConfidenceFeatureProvider: FeatureProvider {
-    public var metadata: ProviderMetadata
+    public static let providerId: String = "SDK_ID_SWIFT_PROVIDER"
+
+    public var metadata: ProviderMetadata = Metadata()
     public var hooks: [any Hook] = []
     private let lock = UnfairLock()
     private let initializationStrategy: InitializationStrategy
@@ -20,8 +22,47 @@ public class ConfidenceFeatureProvider: FeatureProvider {
     private var currentResolveTask: Task<Void, Never>?
     private let confidenceFeatureProviderQueue = DispatchQueue(label: "com.provider.queue")
 
-    /// Initialize the Provider via a `Confidence` object.
-    public convenience init(confidence: Confidence, initializationStrategy: InitializationStrategy = .fetchAndActivate) {
+    /**
+    Creates the `Confidence` object to be used as init parameter for this Provider.
+    */
+    public static func createConfidence(clientSecret: String) -> ConfidenceForOpenFeature {
+        return ConfidenceForOpenFeature.init(confidence: Confidence.Builder.init(clientSecret: clientSecret)
+            .withRegion(region: .global)
+            .withMetadata(metadata: ConfidenceMetadata.init(
+                name: providerId,
+                version: "0.2.1") // x-release-please-version
+            )
+            .build())
+    }
+
+    /**
+    Proxy holder to ensure correct Confidence configuration passed into the Provider's init.
+    Do not instantiate directly.
+    */
+    public class ConfidenceForOpenFeature {
+        internal init(confidence: Confidence) {
+            self.confidence = confidence
+        }
+        let confidence: Confidence
+    }
+
+    /**
+    Initialize the Provider via a `Confidence` object.
+    The `Confidence` object must be creted via the `createConfidence` function available from this same class,
+    rather then be instantiated directly via `Confidence.init(...)` as you would if not using the OpenFeature integration.
+    */
+    public convenience init(
+        confidenceForOF: ConfidenceForOpenFeature,
+        initializationStrategy: InitializationStrategy = .fetchAndActivate
+    ) {
+        self.init(confidence: confidenceForOF.confidence, session: nil)
+    }
+
+    // Allows to pass a confidence object with injected configurations for testing
+    internal convenience init(
+        confidence: Confidence,
+        initializationStrategy: InitializationStrategy = .fetchAndActivate
+    ) {
         self.init(confidence: confidence, session: nil)
     }
 
@@ -30,10 +71,6 @@ public class ConfidenceFeatureProvider: FeatureProvider {
         initializationStrategy: InitializationStrategy = .fetchAndActivate,
         session: URLSession?
     ) {
-        let metadata = ConfidenceMetadata(
-            name: "SDK_ID_SWIFT_PROVIDER",
-            version: "0.2.1") // x-release-please-version
-        self.metadata = Metadata(name: metadata.name)
         self.initializationStrategy = initializationStrategy
         self.confidence = confidence
     }
