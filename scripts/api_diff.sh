e3ee9133d7f3d7b6ef1fa87ada53a69ad1d2b6f1
Nicklas Lundin <nicklasl@spotify.com>
2024-07-03 17:37:29 +0200

    ci: add more info on rememdy

--- scripts/api_diff.sh
+++ scripts/api_diff.sh
@@ -15,5 +15,28 @@ python3 $script_dir/extract_public_funcs.py $script_dir/raw_api.json $script_dir
 rm $script_dir/raw_api.json
 
 # Compare the public API with the previous public API and exit with 1 if there are changes
-# TODO(nicklasl): store the result (exit code) and provide information on how to remedy.
+echo "Comparing genereated public API with previous public API"
+set +e
 git diff --no-index --exit-code $script_dir/public_api.json $script_dir/current_public_api.json
+# Capture the exit code of the git diff command
+diff_exit_code=$?
+set -e
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+printf "\n"
+
+if [ $diff_exit_code -eq 0 ]; then
+    printf "${GREEN}No changes detected in the public API.${NC}"
+else
+    printf "${RED}Changes detected in the public API. Please review the differences.\n
+If the changes are _intended_, please update the public API by running the generate_public_api_list.sh script and commit the changes to public_api.json.\n
+If the changes are unintended, please investigate the changes and update the source code accordingly.${NC}"
+fi
+
+# Clean up the current public api json file
+rm $script_dir/current_public_api.json
+
+# Exit with the diff's exit code to maintain the intended behavior
+exit $diff_exit_code
