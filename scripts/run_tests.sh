cfdb0ff5bcc6984237b9e0b28abee2f8dd579142
Fabrizio Demaria <fabrizio.f.demaria@gmail.com>
2025-07-01 21:59:05 +0200

    ci: Safer Actions setup with opt. e2e tests (#214)
    ci: Safer Actions setup with opt. it tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,17 +5,10 @@ set -e
 script_dir=$(dirname $0)
 root_dir="$script_dir/../"
 
-# Set the client token in your local keychain using the following command:
-# security add-generic-password -s 'swift-provider-e2e'  -a 'confidence-test' -w 'TOKEN'
-# You can then run the script with no arguments - you will need to allow access the first time only.
-test_runner_client_token=$1
-if [[ -z "${test_runner_client_token// }" ]]; then
-    test_runner_client_token=$(security find-generic-password -w -s 'swift-provider-e2e' -a 'confidence-test')
-fi
-
-(cd $root_dir &&
-    TEST_RUNNER_CLIENT_TOKEN=$test_runner_client_token TEST_RUNNER_TEST_FLAG_NAME=$2 xcodebuild \
+(cd $root_dir && \
+    xcodebuild \
         -quiet \
         -scheme Confidence-Package \
         -destination 'platform=iOS Simulator,name=iPhone 15,OS=17.2' \
+        -skip-testing:ConfidenceTests/ConfidenceIntegrationTests \
         test)
