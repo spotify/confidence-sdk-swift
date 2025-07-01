0dc6df9e9f71803a09bba4b71b3248b97b8e515d
Fabrizio Demaria <fabrizio.f.demaria@gmail.com>
2025-07-01 17:10:01 +0200

    ci: Safer Actions setup with opt. it tests

---
+++ scripts/run_integration_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+script_dir=$(dirname $0)
+root_dir="$script_dir/../"
+
+test_runner_client_token=$1
+if [[ -z "${test_runner_client_token// }" ]]; then
+    test_runner_client_token=$(security find-generic-password -w -s 'swift-provider-e2e' -a 'confidence-test')
+fi
+
+(cd $root_dir && \
+    TEST_RUNNER_CLIENT_TOKEN=$test_runner_client_token TEST_RUNNER_TEST_FLAG_NAME=$2 xcodebuild \
+        -quiet \
+        -scheme Confidence-Package \
+        -destination 'platform=iOS Simulator,name=iPhone 15,OS=17.2' \
+        -only-testing:ConfidenceTests/ConfidenceIntegrationTests \
+        test) 
