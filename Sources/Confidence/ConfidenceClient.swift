d90bc2e4ccfd94dec8991cbee4e32d94978bf3ff
Fabrizio Demaria <fabrizio.f.demaria@gmail.com>
2025-07-24 15:59:21 +0200

    fix: ShouldApply backwards compatibility

--- Sources/Confidence/ConfidenceClient.swift
+++ Sources/Confidence/ConfidenceClient.swift
@@ -16,6 +16,30 @@ struct ResolvedValue: Codable, Equatable {
     var flag: String
     var resolveReason: ResolveReason
     var shouldApply: Bool
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        variant = try container.decodeIfPresent(String.self, forKey: .variant)
+        value = try container.decodeIfPresent(ConfidenceValue.self, forKey: .value)
+        flag = try container.decode(String.self, forKey: .flag)
+        resolveReason = try container.decode(ResolveReason.self, forKey: .resolveReason)
+
+        // Default shouldApply to true for backward compatibility when field is missing
+        shouldApply = try container.decodeIfPresent(Bool.self, forKey: .shouldApply) ?? true
+    }
+
+    init(variant: String? = nil, value: ConfidenceValue? = nil, flag: String, resolveReason: ResolveReason, shouldApply: Bool = true) {
+        self.variant = variant
+        self.value = value
+        self.flag = flag
+        self.resolveReason = resolveReason
+        self.shouldApply = shouldApply
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case variant, value, flag, resolveReason, shouldApply
+    }
 }
 
 public struct ResolvesResult: Codable, Equatable {
