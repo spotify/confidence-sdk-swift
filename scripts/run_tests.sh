c9f0495a8f64c0164ca401db6c9b8a9220979584
Fabrizio Demaria <fabrizio.f.demaria@gmail.com>
2025-11-12 16:04:45 +0100

    test: Try to fix CI test failures

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -7,7 +7,6 @@ root_dir="$script_dir/../"
 
 (cd $root_dir && \
     xcodebuild \
-        -quiet \
         -scheme Confidence-Package \
         -destination 'platform=iOS Simulator,name=iPhone 16 Pro,OS=18.5' \
         -skip-testing:ConfidenceTests/ConfidenceIntegrationTests \
